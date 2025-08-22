3180bdbe03e967b5f4fa698eaa640b635a6306a4
the6coders <ganiyuojorufai@gmail.com>
2025-08-22 21:57:59 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js,ts,jsx,tsx}', // This tells Tailwind to scan ALL html, js, ts, jsx, tsx files in ALL subdirectories
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'square-in-center': {
+          'from': { 'clip-path': 'inset(100% 100% 100% 100%)' },
+          'to': { 'clip-path': 'inset(0 0 0 0)' },
+        }
+      },
+      animation: {
+        'square-in-center': 'square-in-center 2.5s cubic-bezier(.25, 1, .30, 1) both',
+      },
+    },
+  },
+  plugins: [],
+}
